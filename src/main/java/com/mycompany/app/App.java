e09ec11838eef7969f2030e1591f94591f5e2306
vladimir.glayzer <its_a_vio@hotmail.com>
2024-04-01 15:12:43 +0300

    auto_push

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello avova!";
+    private static final String MESSAGE = "Hello world!";
 
     public App() {}
 
